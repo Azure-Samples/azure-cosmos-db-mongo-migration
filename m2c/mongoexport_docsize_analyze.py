ef15a7fea79a9662e34bf7b5bd481dff3a4c9fc3
Chris Joakim <christopher.joakim@gmail.com>
2021-11-05 11:08:12 -0400

    Implemented m2c/mongoexport_docsize_analyze.py and .sh scripts

--- m2c/mongoexport_docsize_analyze.py
+++ m2c/mongoexport_docsize_analyze.py
@@ -9,11 +9,6 @@ __email__   = "chjoakim@microsoft.com"
 __license__ = "MIT"
 __version__ = "November 2021"
 
-# Links:
-# https://pandas.pydata.org/
-# https://matplotlib.org/
-# https://pandas.pydata.org/docs/getting_started/intro_tutorials/04_plotting.html
-
 import os
 import sys
 import time
@@ -21,32 +16,59 @@ import traceback
 
 import numpy as np
 import pandas as pd
-import matplotlib as mpl
-import matplotlib.pyplot as plt
 
 from docopt import docopt
-
-plt.style.use('classic')
 
 csv_file = 'tmp/docsize_extract.csv'
 
 def docsize_extract(infile):
     # read infile line-by-line
-    # create a corresponding csv outfile with to fields - row_index and doc_size
+    # create a corresponding csv outfile with three fields; row_index, doc_size, large_ind
     # then, docsize_analysis() will read the csv file and process it with pandas
-    print('docsize_extract: {}'.format(infile))
+    print('docsize_extract: {}'.format(infile), file=sys.stderr)
+    max_doc_size = megabyte() * 2
+    print('max_doc_size: {}'.format(max_doc_size), file=sys.stderr)
+
+    print('row_index,doc_size,large_ind')  # redirected csv header row
+
+    it = text_file_iterator(infile)
+    for i, line in enumerate(it):
+        size = len(line)
+        large_ind = 0
+        if size > max_doc_size:
+            large_ind = 1
+        print('{},{},{}'.format(i, size, large_ind))  # redirected csv detail row
 
 def docsize_analysis():
-    print('docsize_analysis: {}'.format(csv_file))
+    print('docsize_analysis: {}'.format(csv_file), file=sys.stderr)
 
+    df = pd.read_csv(csv_file, sep=',', header=0)
+
+    large_df = df[df['large_ind'] > 0] 
+    large_doc_count = large_df.shape[0]
+
+    print('row/doc count:   {}'.format(df.shape[0]), file=sys.stderr)
+    print('large doc count: {}'.format(large_doc_count), file=sys.stderr)
+    print('max doc size:    {}'.format(df['doc_size'].max()), file=sys.stderr)
+    print('avg doc size:    {}'.format(df['doc_size'].mean()), file=sys.stderr)
+    print('min doc size:    {}'.format(df['doc_size'].min()), file=sys.stderr)
+
+def text_file_iterator(infile):
+    # return a line generator that can be iterated with iterate()
+    with open(infile, 'rt') as f:
+        for line in f:
+            yield line.strip()
 
 def describe_df(df, msg):
-    print('=== describe df: {}'.format(msg))
+    print('=== describe df: {}'.format(msg), file=sys.stderr)
     print(str(type(df)))  # <class 'pandas.core.frame.DataFrame'>
-    print('--- df.dtypes')
+    print('--- df.dtypes', file=sys.stderr)
     print(df.dtypes)
-    print('--- df.shape')
+    print('--- df.shape', file=sys.stderr)
     print(df.shape)
+
+def megabyte():
+    return pow(1024, 2)
 
 def print_options(msg):
     print(msg)
@@ -68,41 +90,3 @@ if __name__ == "__main__":
             print_options('Error: invalid command-line function: {}'.format(func))
     else:
         print_options('Error: no command-line function given')
-
-
-# Sample Output:
-#
-# $ python mongoexport_analyze.py pk_analysis raw_data/mongoexport/openflights__routes.json
-# mongoexport_pk_analyze: raw_data/mongoexport/openflights__routes.json
-# === describe df: mongoexport file: raw_data/mongoexport/openflights__routes.json
-# <class 'pandas.core.frame.DataFrame'>
-# --- df.dtypes
-# _id                         object
-# airline_id                  object
-# openflights_airline_id      object
-# from_airport                object
-# openflights_from_airport    object
-# to_airport                  object
-# openflights_to_airport      object
-# x                           object
-# codeshare                    int64
-# equipment                   object
-# dtype: object
-# --- df.shape
-# (67663, 10)
-#                                     _id airline_id openflights_airline_id  ... x codeshare equipment
-# 0  {'$oid': '6166d5927a8e7d4fa3497300'}         2B                    410  ...           0       CR2
-# 1  {'$oid': '6166d5927a8e7d4fa3497301'}         2B                    410  ...           0       CR2
-# 2  {'$oid': '6166d5927a8e7d4fa3497302'}         2B                    410  ...           0       CR2
-# 3  {'$oid': '6166d5927a8e7d4fa3497303'}         2B                    410  ...           0       CR2
-# 4  {'$oid': '6166d5927a8e7d4fa3497304'}         2B                    410  ...           0       CR2
-
-# [5 rows x 10 columns]
-# attribute 'airline_id' has 568 unique values
-# plot saved to file plots/airline_id.png
-# attribute 'from_airport' has 3409 unique values
-# plot saved to file plots/from_airport.png
-# attribute 'to_airport' has 3418 unique values
-# plot saved to file plots/to_airport.png
-# attribute 'equipment' has 3946 unique values
-# plot saved to file plots/equipment.png
--- m2c/mongoexport_docsize_analyze.sh
+++ m2c/mongoexport_docsize_analyze.sh
@@ -6,11 +6,39 @@
 
 source env.sh
 
-# manually edit 'infile', on the next line:
-export infile=$M2C_APP_DATA_DIR/mongoexports/openflights/openflights__routes.json
+mkdir -p tmp/
 
-python mongoexport_docsize_analyze.py docsize_extract  $infile
+# manually edit 'infile', on the next line:
+export infile=$M2C_APP_DATA_DIR/mongoexports/olympics/olympics__g2016_summer.json
+
+python mongoexport_docsize_analyze.py docsize_extract $infile > tmp/docsize_extract.csv
+
+echo 'head of file tmp/docsize_extract.csv:'
+head  tmp/docsize_extract.csv
 
 python mongoexport_docsize_analyze.py docsize_analysis
 
 echo 'done'
+
+# Sample Output:
+# $ ./mongoexport_docsize_analyze.sh
+# docsize_extract: /Users/cjoakim/github/azure-cosmos-db-mongo-migration/reference_app/data/mongoexports/olympics/olympics__g2016_summer.json
+# max_doc_size: 2097152
+# head of file tmp/docsize_extract.csv:
+# row_index,doc_size,large_ind
+# 0,340,0
+# 1,330,0
+# 2,343,0
+# 3,341,0
+# 4,347,0
+# 5,330,0
+# 6,337,0
+# 7,345,0
+# 8,323,0
+# docsize_analysis: tmp/docsize_extract.csv
+# row/doc count:   13688
+# large doc count: 0
+# max doc size:    387
+# avg doc size:    336.8294126241964
+# min doc size:    306
+# done
